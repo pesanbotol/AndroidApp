348695d0dc85eaf463f62b3bdc0261ef39ebaf9b
cekakaka <dwicandra920@gmail.com>
2022-05-17 12:48:01 +0700

    fixing auth

--- app/src/main/java/com/dwicandra/pesanbotol/LoginActivity.kt
+++ app/src/main/java/com/dwicandra/pesanbotol/LoginActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.dwicandra.pesanbotol.databinding.ActivityLoginBinding
-import com.dwicandra.pesanbotol.databinding.ActivitySignUpBinding
 
 class LoginActivity : AppCompatActivity() {
 
