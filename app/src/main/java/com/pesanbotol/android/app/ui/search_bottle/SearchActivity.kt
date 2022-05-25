ec291fd0d891f1aa04db95eac846b27ec34f1363
cekakaka <dwicandra920@gmail.com>
2022-05-25 12:08:11 +0700

    fixing search bar

---
+++ app/src/main/java/com/pesanbotol/android/app/ui/search_bottle/SearchActivity.kt
@@ -0,0 +1,12 @@
+package com.pesanbotol.android.app.ui.search_bottle
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.pesanbotol.android.app.R
+
+class SearchActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search)
+    }
+}
